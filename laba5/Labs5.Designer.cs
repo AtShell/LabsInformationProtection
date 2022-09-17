36305e5656e5fdf18059d9d1df91f64e0ad1e5f7
Вячеслав Уржумов <s.urzhumov@outlook.com>
2022-09-18 01:30:40 +0500

    Labs5 ready

--- laba5/Labs5.Designer.cs
+++ laba5/Labs5.Designer.cs
@@ -37,11 +37,14 @@
             this.BinaryOutputMessage = new System.Windows.Forms.TextBox();
             this.OutputMessage = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.ErrorArray = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // InputMessage
             // 
-            this.InputMessage.Location = new System.Drawing.Point(11, 77);
+            this.InputMessage.Location = new System.Drawing.Point(14, 77);
             this.InputMessage.Name = "InputMessage";
             this.InputMessage.Size = new System.Drawing.Size(274, 23);
             this.InputMessage.TabIndex = 0;
@@ -50,7 +53,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(59, 59);
+            this.label1.Location = new System.Drawing.Point(62, 59);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(128, 15);
             this.label1.TabIndex = 1;
@@ -59,7 +62,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(407, 59);
+            this.label2.Location = new System.Drawing.Point(424, 59);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(143, 15);
             this.label2.TabIndex = 3;
@@ -67,16 +70,16 @@
             // 
             // BinaryInputMessage
             // 
-            this.BinaryInputMessage.Location = new System.Drawing.Point(11, 127);
+            this.BinaryInputMessage.Location = new System.Drawing.Point(14, 127);
             this.BinaryInputMessage.Multiline = true;
             this.BinaryInputMessage.Name = "BinaryInputMessage";
-            this.BinaryInputMessage.Size = new System.Drawing.Size(274, 62);
+            this.BinaryInputMessage.Size = new System.Drawing.Size(274, 170);
             this.BinaryInputMessage.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(59, 109);
+            this.label3.Location = new System.Drawing.Point(62, 109);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(147, 15);
             this.label3.TabIndex = 5;
@@ -85,7 +88,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(391, 109);
+            this.label4.Location = new System.Drawing.Point(408, 109);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(147, 15);
             this.label4.TabIndex = 8;
@@ -93,15 +96,15 @@
             // 
             // BinaryOutputMessage
             // 
-            this.BinaryOutputMessage.Location = new System.Drawing.Point(343, 127);
+            this.BinaryOutputMessage.Location = new System.Drawing.Point(360, 127);
             this.BinaryOutputMessage.Multiline = true;
             this.BinaryOutputMessage.Name = "BinaryOutputMessage";
-            this.BinaryOutputMessage.Size = new System.Drawing.Size(274, 62);
+            this.BinaryOutputMessage.Size = new System.Drawing.Size(274, 170);
             this.BinaryOutputMessage.TabIndex = 7;
             // 
             // OutputMessage
             // 
-            this.OutputMessage.Location = new System.Drawing.Point(343, 77);
+            this.OutputMessage.Location = new System.Drawing.Point(360, 77);
             this.OutputMessage.Name = "OutputMessage";
             this.OutputMessage.Size = new System.Drawing.Size(274, 23);
             this.OutputMessage.TabIndex = 6;
@@ -109,18 +112,50 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(44, 9);
+            this.label5.Location = new System.Drawing.Point(170, 9);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(495, 30);
+            this.label5.Size = new System.Drawing.Size(573, 30);
             this.label5.TabIndex = 9;
-            this.label5.Text = "Реализовать проверку  правильности передачи двоичной кодовой последовательности \r" +
-    "\nдлиной 8 бит, используя метод кода с одиночным битом четности. (11)";
+            this.label5.Text = "Реализовать проверку  правильности передачи двоичной кодовой последовательности д" +
+    "линой 8 бит, \r\n                                             используя метод кода" +
+    " с одиночным битом четности. (11)";
+            // 
+            // ErrorArray
+            // 
+            this.ErrorArray.Location = new System.Drawing.Point(640, 77);
+            this.ErrorArray.Multiline = true;
+            this.ErrorArray.Name = "ErrorArray";
+            this.ErrorArray.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ErrorArray.Size = new System.Drawing.Size(240, 221);
+            this.ErrorArray.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(640, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(158, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Статус полученных блоков";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(291, 127);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 42);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Создать помехи\r\n";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Labs5
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(893, 311);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.ErrorArray);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.BinaryOutputMessage);
@@ -149,5 +184,8 @@
         private TextBox BinaryOutputMessage;
         private TextBox OutputMessage;
         private Label label5;
+        private TextBox ErrorArray;
+        private Label label6;
+        private Button button2;
     }
 }
--- laba5/Labs5.cs
+++ laba5/Labs5.cs
@@ -5,13 +5,17 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace LabsInformationProtection.laba5
 {
     public partial class Labs5 : Form
     {
+        string[,] errors;
+        bool interferences = false;
         public Labs5()
         {
             InitializeComponent();
@@ -22,20 +26,33 @@ namespace LabsInformationProtection.laba5
             MainForm mf = new MainForm();
             mf.Show();
         }
-
-        private void InputMessage_TextChanged(object sender, EventArgs e)
+        private string CheckString(char c)
+        {
+            string res = Convert.ToString(c, 2);
+            while (res.Length < 8)
+                res = "0" + res;
+            return res;
+        }
+        private void ContinuousTransmission()
         {
             BinaryInputMessage.Text = "";
             foreach (var v in InputMessage.Text)
-                BinaryInputMessage.Text += Convert.ToString(v,2)+$"[{AddControlBit(Convert.ToString(v, 2))}]" +"  ";
+                BinaryInputMessage.Text += CheckString(v) + $"[{AddControlBit(Convert.ToString(v, 2))}]" + " ";
+            BinaryOutputMessage.Text = Decrypt(BinaryInputMessage.Text);
+            ListErrors();
+        }
+        private void InputMessage_TextChanged(object sender, EventArgs e)
+        {
+            if ((!Regex.IsMatch(InputMessage.Text, @"^[a-zA-Z0-9 ]+$"))&&InputMessage.Text.Length>0)
+                MessageBox.Show("Доступны только 0-9 и a-z(A-Z)");
+            else
+                ContinuousTransmission();
         }
         private string AddControlBit(string s)
         {
             int[] mass = s.Select(ch => int.Parse(ch.ToString())).ToArray();
             int[] mass1 = new int[mass.Length];
             mass1 = mass;
-
-
             int count = 0;
             string result = "";
             for (int i = 0; i < mass.Length; i++)
@@ -45,6 +62,103 @@ namespace LabsInformationProtection.laba5
             else
                 result = Convert.ToString(count);
             return result;
+        }
+        private string ControlBitDecoding(string str)
+        {
+            int[] mass = str.Select(ch => int.Parse(ch.ToString())).ToArray();
+            int a = Convert.ToInt16(mass[mass.Length - 1]);
+            int[] mass1 = new int[mass.Length];
+            mass1 = mass;
+            int count = 0;
+            int result;
+            for (int i = 0; i < mass.Length - 1; i++)
+                count += mass[i] * mass1[i];
+            if (count > 1)
+                result = count % 2;
+            else
+                result = count;
+            if (result == a)
+                return "Нет ошибки";
+            else
+                return "Ошибка";
+        }
+        private string Interferences(string input)
+        {
+            string temp = "";
+            Random rand = new Random();
+            int error = rand.Next(0, 2);
+            foreach (var v in input)
+            {
+                if (v != ' ')
+                {
+                    if (v == '1' && error == 1)
+                    {
+                        temp += 0;
+                        error = 0;
+                    }
+                    else
+                        temp += v;
+                }
+                else
+                {
+                    temp += " ";
+                    error = rand.Next(0, 2);
+                }
+            }
+            return temp;
+        }
+        private string Decrypt(string encryptedText)
+        {
+            if (interferences == true)
+                encryptedText = Interferences(encryptedText);
+            int count = 1;
+            foreach (var v in encryptedText)
+                if (v == ' ')
+                    count++;
+            errors = new string[count - 1, 2];
+            string result = "";
+            string temp = "";
+            int i = 0;
+            foreach (var v in encryptedText)
+            {
+                if (v != ' ')
+                {
+                    if (v != '[' && v != ']')
+                        temp += v;
+                }
+                else
+                {
+                    var strArr = Enumerable.Range(0, temp.Length / 8).Select(i => Convert.ToByte(temp.Substring(i * 8, 8), 2)).ToArray();
+                    result += Encoding.UTF8.GetString(strArr);
+                    errors[i, 0] = temp;
+                    errors[i, 1] = ControlBitDecoding(temp);
+                    i++;
+                    temp = "";
+                }
+            }
+            OutputMessage.Text = result;
+            return encryptedText;
+        }
+        private void ListErrors()
+        {
+            ErrorArray.Text = "";
+            for (int i = 0; i < errors.Length / 2; i++)
+                ErrorArray.Text += $"[{i + 1}] {errors[i, 0]} => {errors[i, 1]}{Environment.NewLine}";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "Создать помехи\r\n")
+            {
+                interferences = true;
+                button2.Text = "Убрать помехи";
+            }
+            else
+            {
+                interferences = false;
+                button2.Text = "Создать помехи\r\n";
+            }
+            ContinuousTransmission();
         }
     }
 }
