d2246ad70ffde7824488142a53267f9250e8bafb
Вячеслав Уржумов <s.urzhumov@outlook.com>
2022-09-19 16:57:02 +0500

    Start 7labs

--- Laba7/Labs7.Designer.cs
+++ Laba7/Labs7.Designer.cs
@@ -28,20 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Labs7));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(634, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(35, 72);
+            this.textBox1.MaxLength = 28;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PlaceholderText = "Пароль для хеширования";
+            this.textBox1.Size = new System.Drawing.Size(318, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(35, 101);
+            this.textBox2.MaxLength = 28;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PlaceholderText = "Хешированный пароль";
+            this.textBox2.Size = new System.Drawing.Size(318, 23);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(359, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Хешировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Labs7
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Name = "Labs7";
-            this.Text = "Labs7";
+            this.Text = "Лабораторная работа 7";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Labs7_FormClosed);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
     }
 }
--- Laba7/Labs7.cs
+++ Laba7/Labs7.cs
@@ -12,6 +12,16 @@ namespace LabsInformationProtection.Laba7
 {
     public partial class Labs7 : Form
     {
+        char[] alphabet =
+        {
+            '0','1','2','3','4','5','6','7','8','9','.',',','?','!','/','*','-','+','=',
+            'а','б','в','г','д','е','ё','ж','з','и','к','л','м','н','о','п','р','с',
+            'т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я',
+            'А','Б','В','Г','Д','Е','Ё','Ж','З','И','К','Л','М','Н','О','П','Р','С',
+            'Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я',
+            'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+            'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+        };
         public Labs7()
         {
             InitializeComponent();
@@ -21,6 +31,28 @@ namespace LabsInformationProtection.Laba7
         {
             MainForm mf=new MainForm();
             mf.Show();
+        }
+        private string GetHash()
+        {
+            //using labs4 for hashing password
+            return "";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+                textBox1.Text = GeneratePassword();
+            else
+                textBox2.Text=GetHash();
+        }
+        private string GeneratePassword()
+        {
+            string result = "";
+            while (result.Length != 28)
+            {
+                Random random1 = new Random();
+                result += alphabet[random1.Next(0, alphabet.Length)];
+            }
+            return result;
         }
     }
 }
